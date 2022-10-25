4597fe0efe63a1e07c27f2423900d12afb2b3bdf
Krypton <k.mohamed.11.98@gmail.com>
2022-10-25 07:15:52 +0200

    ADD about comp & EDIT navbar right media query

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import "./index.scss";
 
 function About() {
     return (
-        <div>
-            About
-        </div>
+        <section className="about" id="about">
+            <div className="container">
+                <div className="row">
+                    <h2 className="about__title">about me</h2>
+
+                    <div className="about__info">
+                        <div className="about__info__pic">
+                            <img src="./assets/images/person/pic-2-min.jpg" alt="Person in the about section" />
+                        </div>
+
+                        <div className="about__info__desc">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, consectetur dolor. Neque minus obcaecati sunt nisi id atque et nulla sint eum harum. Enim itaque distinctio perferendis, nihil natus, facere eveniet aut mollitia ex architecto, cumque ipsum suscipit odio sunt. Sunt, deserunt vel eaque illum soluta voluptatibus libero non dolores?
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
