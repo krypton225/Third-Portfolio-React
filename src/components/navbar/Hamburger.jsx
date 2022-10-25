f1dc83bf50b83cb26bb1491b0388eb715fe1aaf9
Krypton <k.mohamed.11.98@gmail.com>
2022-10-25 08:29:58 +0200

    ADD portfolio comp & EDIT hamburger menu

--- src/components/navbar/Hamburger.jsx
+++ src/components/navbar/Hamburger.jsx
@@ -1,7 +1,7 @@
 function Hamburger({ isActiveComponent, toggleClass }) {
     return (
         <>
-            <div className={`navbar__hamburger ${isActiveComponent ? "active" : ""}`} onClick={toggleClass}>
+            <div className={`navbar__hamburger tap-light-remove ${isActiveComponent ? "active" : ""}`} onClick={toggleClass}>
                 <span></span>
                 <span></span>
                 <span></span>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,41 @@
-import "./portfolio.scss"
+import "./index.scss";
 
 function Portfolio() {
     return (
-        <section className="portfolio">
-            Portfolio
+        <section className="portfolio" id="portfolio">
+            <div className="container">
+                <div className="row">
+
+                    <h2 className="portfolio__title">portfolio</h2>
+
+                    <div className="portfolio__info">
+                        <div className="portfolio__info__product">
+                            <img src="./assets/images/portfolio/pic-1.jpg" alt="Product 1" />
+                            <div className="portfolio__info__product__desc">
+                                <h4 className="portfolio__info__product__desc__title">product 1</h4>
+                                <h5 className="portfolio__info__product__desc__category">web design</h5>
+                            </div>
+                        </div>
+
+                        <div className="portfolio__info__product">
+                            <img src="./assets/images/portfolio/pic-2.jpg" alt="Product 2" />
+                            <div className="portfolio__info__product__desc">
+                                <h4 className="portfolio__info__product__desc__title">product 2</h4>
+                                <h5 className="portfolio__info__product__desc__category">web development</h5>
+                            </div>
+                        </div>
+
+                        <div className="portfolio__info__product">
+                            <img src="./assets/images/portfolio/pic-3.jpg" alt="Product 3" />
+                            <div className="portfolio__info__product__desc">
+                                <h4 className="portfolio__info__product__desc__title">product 3</h4>
+                                <h5 className="portfolio__info__product__desc__category">UI / UX</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     )
 }
